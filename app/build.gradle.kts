ed0fba354ae5f21ffb2fdc5c35c764d166a2f2ab
ElianaCepeda <eli.cepedagonzalez@gmail.com>
2025-02-22 15:50:57 -0500

    Pantalla principal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.taller1movil"
     compileSdk = 35
 
+    buildFeatures {
+        viewBinding=true
+    }
+
     defaultConfig {
         applicationId = "com.example.taller1movil"
         minSdk = 29
